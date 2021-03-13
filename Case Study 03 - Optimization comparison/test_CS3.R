3705cb9816c39028fcb5b2d3b36396dcae1e0152
Lucas Carneiro <32558976+carneiroll@users.noreply.github.com>
2021-03-13 19:30:01 -0300

    Fiz um código para a análise após ter os resultados das instancias

--- Case Study 03 - Optimization comparison/test_CS3.R
+++ Case Study 03 - Optimization comparison/test_CS3.R
@@ -1,5 +1,19 @@
-Y_est <- read.csv(".\\data\\param_data.csv", sep=';', dec = ",", header = TRUE)
-Y_est <- data.frame(Y_est)
-model <- aov(resultado~bloco+config, data=Y_est)
-summary(model)
+library(CAISEr)
+library(multcomp)
+
+# Número de instancias
 Ncalc <- calc_instances(1,power = 0.8, d = 0.5, sig.level = 0.05, alternative = "two.sided", test = "t.test")
+
+# Análise Anova 
+mdl <- aov(resultado~config+instancia, data = Y)
+summary(mdl)
+
+# Não tenho certeza, algum gráfico kk
+par(mfrow = c(2, 2))
+plot(mdl, pch = 20, las = 1)
+
+# Comparação Múltipla (caso dê diferença)
+mdl$model$config = as.factor(config)
+dtest <- glht(model, linfct = mcp(config = "Dunnett"))
+dtestIC <- confint(dtest,level = 0.95)
+plot(dtestIC)
