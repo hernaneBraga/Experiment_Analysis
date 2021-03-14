5d076d85b87999877932ba484874a3e6d19cfba9
Lucas Carneiro <32558976+carneiroll@users.noreply.github.com>
2021-03-14 20:10:54 -0300

    Análise anova
    Modifiquei o código test_CS3, onde gera os testes anova para os dados normais e em escala log

--- Case Study 03 - Optimization comparison/data_tabulation.R
+++ Case Study 03 - Optimization comparison/data_tabulation.R
@@ -2,14 +2,14 @@ fulldata <- read.csv2(".\\data\\inst_data_complete.csv", sep=';', header = TRUE)
 aggdata <- with(fulldata, aggregate(x=resultado,
                                     by=list(config,instancia),
                                     FUN=mean))
-names(aggdata) <- c("Configuração","Instância","Resultado")
+names(aggdata) <- c("Configuracao","Instancia","Resultado")
 for(i in 1:2) aggdata[,i] <- as.factor(aggdata[,i])
-levels(aggdata$Configuração) <- c("Config1","Config2")
+levels(aggdata$Configuracao) <- c("Config1","Config2")
 summary(aggdata)
 
 library(ggplot2)
-p <- ggplot(aggdata, aes(x = Instância, 
+p <- ggplot(aggdata, aes(x = Instancia, 
                          y = Resultado, 
-                         group = Configuração, 
-                         colour = Configuração))
+                         group = Configuracao, 
+                         colour = Configuracao))
 p + geom_line(linetype = 2) + geom_point(size=5)
--- Case Study 03 - Optimization comparison/test_CS3.R
+++ Case Study 03 - Optimization comparison/test_CS3.R
@@ -4,16 +4,44 @@ library(multcomp)
 # Número de instancias
 Ncalc <- calc_instances(1,power = 0.8, d = 0.5, sig.level = 0.05, alternative = "two.sided", test = "t.test")
 
+# "Tratamento" dos Dados
+fulldata <- read.csv2(".\\data\\inst_data_complete.csv", sep=';', header = TRUE)
+aggdata <- with(fulldata, aggregate(x=resultado,
+                                    by=list(config,instancia),
+                                    FUN=mean))
+names(aggdata) <- c("Configuracao","Instancia","Resultado")
+for(i in 1:2) aggdata[,i] <- as.factor(aggdata[,i])
+levels(aggdata$Configuracao) <- c("Config1","Config2")
+summary(aggdata)
+
+library(ggplot2)
+p <- ggplot(aggdata, aes(x = Instancia, 
+                         y = Resultado, 
+                         group = Configuracao, 
+                         colour = Configuracao))
+p + geom_line(linetype = 2) + geom_point(size=5)
+
 # Análise Anova 
-mdl <- aov(resultado~config+instancia, data = Y)
-summary(mdl)
+mdl1 <- aov(Resultado~Configuracao+Instancia, data = aggdata)
+aggdata$Resultado[1] <- 0.0001 # Log de 0 dá -inf
+mdl2 <- aov(log(Resultado)~Configuracao+Instancia, data = aggdata)
+summary(mdl1)
+summary(mdl2)
 
 # Não tenho certeza, algum gráfico kk
 par(mfrow = c(2, 2))
-plot(mdl, pch = 20, las = 1)
+plot(mdl1, pch = 20, las = 1)
+par(mfrow = c(2, 2))
+plot(mdl2, pch = 20, las = 1)
 
 # Comparação Múltipla (caso dê diferença)
-mdl$model$config = as.factor(config)
-dtest <- glht(model, linfct = mcp(config = "Dunnett"))
-dtestIC <- confint(dtest,level = 0.95)
-plot(dtestIC)
+dtest1 <- glht(mdl1, linfct = mcp(Configuracao = "Dunnet"))
+dtestIC1 <- confint(dtest1,level = 0.95)
+par(mar = c(5, 8, 4, 2), las = 1)
+plot(dtestIC1,xlim = c(-10^7,10^7))
+
+dtest2 <- glht(mdl2, linfct = mcp(Configuracao = "Dunnet"))
+dtestIC2 <- confint(dtest2,level = 0.95)
+par(mar = c(5, 8, 4, 2), las = 1)
+plot(dtestIC2,xlim = c(-10,10))
+
