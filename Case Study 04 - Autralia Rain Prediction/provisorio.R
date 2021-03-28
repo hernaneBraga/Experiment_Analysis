620ccc5311812e551fc735881bd5da15fbbf8b6b
Lucas Carneiro <32558976+carneiroll@users.noreply.github.com>
2021-03-28 19:57:09 -0300

    algumas modificações no código

--- Case Study 04 - Autralia Rain Prediction/provisorio.R
+++ Case Study 04 - Autralia Rain Prediction/provisorio.R
@@ -6,7 +6,8 @@ fulldata <- read.csv2("teste.csv", sep=',',dec = ".", header = FALSE)
 X <- as.matrix(fulldata[,1:4])
 Y <- as.factor(fulldata[,5])
 
-idx <- createDataPartition(Y,p = 0.7,  list = FALSE)
+#idx <- createDataPartition(Y,p = 0.7,  list = FALSE)
+idx <- createTimeSlices(Y, 100, horizon = 100, fixedWindow = TRUE, skip = 0)
 
 X_train <- as.matrix(fulldata[idx,1:4])
 Y_train <- as.factor(fulldata[idx,5])
@@ -17,9 +18,13 @@ Y_test <- as.factor(fulldata[-idx,5])
 # Treinamento dos Modelos
 
 # Random Forest
-randomf <- train(X_train, Y_train, method = 'ranger', 
-               preProcess = 'scale',trControl = trainControl(method = "cv", number = 5))
-print(model)
+# randomf <- train(X_train, Y_train, method = 'ranger',
+#                preProcess = 'scale',trControl = trainControl(method = "cv", number = 5))
+randomf <- train(X_train, Y_train, method = 'ranger',
+                 preProcess = 'scale',
+                 trControl = trainControl(method = "timeslice",initialWindow = 400,
+                                          horizon = 400, fixedWindow = TRUE, skip = 0))
+print(randomf)
 
 # Multi Layer Perceptron
 mlperceptron <- train(X_train, Y_train, method = 'mlp', 
@@ -36,6 +41,11 @@ fuzzy <- train(X_train, Y_train, method = 'FRBCS.CHI',
 print(fuzzy)
 
 # Erro dos modelos
-yhat <- predict.train(svm, newdata = X_test)
+models <- list(randomf, mlperceptron, svmrbf, fuzzy)
+yhat <- predict.train(models[[1]], newdata = X_test)
 e <- mean(as.numeric(yhat) == as.numeric(Y_test))
+
+# yhat <- predict(mlperceptron,X_test)
+# e <- postResample(pred = yhat, obs = Y_test)
+# e <- e[1];
 
